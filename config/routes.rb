6313a80e9b94a178cbc6f557cfb83767a92bddc5
decknoboh <135430121+decknoboh@users.noreply.github.com>
2023-07-09 22:25:01 +0000

    [Finish] 3-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  get 'lists/index'
+  get '/top' => 'homes#top'
+  post 'lists' => 'lists#create'
+  get 'lists' => 'lists#index'
   get 'lists/show'
   get 'lists/edit'
-  get '/top' => 'homes#top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  post 'lists' => 'lists#create'
 end
